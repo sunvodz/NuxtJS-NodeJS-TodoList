a4ae7cc89f776feda3639fb610d9223139cbb71a
SunvoDz <42707869+sunvodz@users.noreply.github.com>
2020-01-21 14:07:25 +0700

    Check

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,8 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: ['~/plugins/moment.js'],
+  plugins: ['~/plugins/moment.js', '~/plugins/datePicker.js'
+  ],
   /*
   ** Nuxt.js dev-modules
   */
--- pages/helloWorld.vue
+++ pages/helloWorld.vue
@@ -7,6 +7,7 @@
       <div class="links">
         <buttonComponent />
         <p>{{ dateTime }}</p>
+        <date-picker placeholder="select date" format="DD/MM/YYYY" />
       </div>
     </div>
   </div>
